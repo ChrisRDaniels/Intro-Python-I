4964f63fc55706da12379d16b415e2bd25756ae8
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2020-08-07 17:27:53 -0600

    push efore 1:1

--- src/12_scopes.py
+++ src/12_scopes.py
@@ -10,8 +10,8 @@ def change_x():
 change_x()
 
 # This prints 12. What do we have to modify in change_x() to get it to print 99?
-print(x)
 
+print(x)
 
 # This nested function has a similar problem.
 
@@ -26,6 +26,7 @@ def outer():
     # This prints 120. What do we have to change in inner() to get it to print
     # 999?
     # Note: Google "python nested function scope".
+
     print(y)
 
 
